75792199e9448c86107c314c66f933d3745fda62
nvolodin <nick_greed@mail.ru>
2015-03-05 13:15:53 +0400

    set timeout for karma

--- karma.conf.js
+++ karma.conf.js
@@ -2,13 +2,12 @@ module.exports=function(config){
 	config.set({
 		basePath : './',
 		files : [
-			'app/components/jquery/dist/jquery.js',
-			'app/components/angular/angular.js',
-			'app/components/angular-route/angular-route.js',
+			'https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js',
+			'https://ajax.googleapis.com/ajax/libs/angularjs/1.3.14/angular.min.js',
+			'https://ajax.googleapis.com/ajax/libs/angularjs/1.3.14/angular-route.min.js',
 			'app/components/angular-mocks/angular-mocks.js',
 			'https://cdn.socket.io/socket.io-1.3.2.js',
-			'app/js/*.js',
-			'app/js/**/*.js',
+			'app/js/app.min.js',
 			'test/unit/**/*.js'
 		],
 		frameworks: ['jasmine'],
