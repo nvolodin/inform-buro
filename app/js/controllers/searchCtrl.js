c54f6750eea19a69ce0d03ef6a7803b3b7bd7f58
nvolodin <nick_greed@mail.ru>
2015-03-26 18:31:21 +0400

    [*] add offers row, change css by search panel

--- app/js/controllers/searchCtrl.js
+++ app/js/controllers/searchCtrl.js
@@ -1,28 +1,47 @@
-(function(){
+(function () {
 
-'use strict';
-/* Controllers */
-angular
-	.module('informBuroApp')
-	.controller('SearchCtrl', SearchController);
-	
-	SearchController.$inject =['webSocket','$routeParams',];
-	function SearchController(webSocket,$routeParams){
-		var vm=this;
-		init();
+    'use strict';
+    /* Controllers */
+    angular
+        .module('informBuroApp')
+        .controller('SearchCtrl', SearchController);
 
-		function init(){
-			if ($routeParams && $routeParams.drug) {
-				var text=decodeURIComponent($routeParams.drug)
-				vm.drugs=[];
-				webSocket.getDrugs(text, addDrug);
-			}
-		}
+    SearchController.$inject = ['webSocket', '$routeParams'];
+    function SearchController(webSocket, $routeParams) {
+        var vm = this;
+        init();
 
-		function addDrug(data){
-			vm.drugs=$.grep(data, function(item){
-				return item.prices;
-			});
-		}
-	}	
+        vm.getDrugOffersCount = function (offersCount) {
+            if (!offersCount)
+                return '';
+            var offersCountStr = offersCount.toString();
+            var lastDigit = !offersCountStr.length ? '0' : offersCountStr[offersCountStr.length - 1],
+                prevDigit = offersCountStr.length > 1 ? offersCountStr[offersCountStr.length - 2] : '0';
+            var postfix = '';
+            if (prevDigit == '1' || lastDigit === '5' || lastDigit === '6' || lastDigit === '7' || lastDigit === '8' || lastDigit === '9' || lastDigit === '0') {
+                postfix = 'й';
+            } else if (lastDigit === '1') {
+                postfix = 'е';
+            } else if (lastDigit === '2' || lastDigit === '3' || lastDigit === '4') {
+                postfix = 'я';
+            } else {
+                return '';
+            }
+            return offersCount + ' предложени' + postfix;
+        };
+
+        function init() {
+            if ($routeParams && $routeParams.drug) {
+                var text = decodeURIComponent($routeParams.drug);
+                vm.drugs = [];
+                webSocket.getDrugs(text, addDrug);
+            }
+        }
+
+        function addDrug(data) {
+            vm.drugs = $.grep(data, function (item) {
+                return item.prices;
+            });
+        }
+    }
 })();
--- app/js/controllers/searchPanelCtrl.js
+++ app/js/controllers/searchPanelCtrl.js
@@ -1,27 +1,27 @@
-(function() {
-	
-'use strict';
-/* Controllers */
-angular
-	.module('informBuroApp')
-	.controller('SearchPanelCtrl', SearchPanelController);
+(function () {
 
-	SearchPanelController.$inject =['$location','$routeParams'];
-	function SearchPanelController($location ,$routeParams){
-		var vm=this;
-		init();
+    'use strict';
+    /* Controllers */
+    angular
+        .module('informBuroApp')
+        .controller('SearchPanelCtrl', SearchPanelController);
 
-		function init(){
-			vm.searchText='';
-			
-			if ($routeParams.drug){
-				vm.searchText=decodeURIComponent($routeParams.drug);
-			}
-		}
+    SearchPanelController.$inject = ['$location', '$routeParams'];
+    function SearchPanelController($location, $routeParams) {
+        var vm = this;
+        init();
 
-		vm.search=function(){
-			if (vm.searchText)
-				$location.path('/search/'+encodeURIComponent(vm.searchText));
-		}
-	}
+        vm.search = function () {
+            if (vm.searchText)
+                $location.path('/search/' + encodeURIComponent(vm.searchText));
+        };
+
+        function init() {
+            vm.searchText = '';
+
+            if ($routeParams.drug) {
+                vm.searchText = decodeURIComponent($routeParams.drug);
+            }
+        }
+    }
 })();
