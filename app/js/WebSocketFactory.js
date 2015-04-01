14e2abbdc31fc552200c582d0a7c89a893c4aceb
nvolodin <nick_greed@mail.ru>
2015-04-01 10:02:59 +0400

    [+] add favorites to webservice

--- app/js/WebSocketFactory.js
+++ app/js/WebSocketFactory.js
@@ -1,54 +1,65 @@
-(function(){
-	'use strict';
+(function () {
+    'use strict';
 
-	angular.module('informBuroApp.core',[])
+    angular.module('informBuroApp.core', [])
 
-	angular.module('informBuroApp.core')
-		.value('$io',io)
-		.factory('webSocket', webSocket);
+    angular.module('informBuroApp.core')
+        .value('$io', io)
+        .factory('webSocket', webSocket);
 
-	webSocket.$inject=['$rootScope', '$io'];
+    webSocket.$inject = ['$rootScope', '$io'];
 
-	function webSocket($rootScope, $io){
-		var service={
-			getDrugs:getDrugs,
-			getDrugStoresByDrug:getDrugStoresByDrug
-		},
-		socket = $io.connect('http:inform-buro.info:80');
+    function webSocket($rootScope, $io) {
+        var service = {
+                getDrugs: getDrugs,
+                getDrugStoresByDrug: getDrugStoresByDrug,
+                getFavorites: getFavorites
+            },
+            socket = $io.connect('http:inform-buro.info:80');
 
-		socket.on('error', function () {
-	            console.log('there was an error');
-	        });
+        socket.on('error', function () {
+            console.log('there was an error');
+        });
 
-		return service;
+        return service;
 
-		function getDrugs(query, callback){
-			 socket.emit('send_gridtable1', ['', query,,,'все','все','','',,,,,,,,,'','']);
-			 socket.on('grid_data1', function () {  
-		        var args = arguments;
-		        $rootScope.$apply(function () {
-		          callback.apply(socket, args);
-		        });
-		      });
-		}
+        function getDrugs(query, callback) {
+            socket.emit('send_gridtable1', ['', query, , , 'все', 'все', '', '', , , , , , , , , '', '']);
+            socket.on('grid_data1', function () {
+                var args = arguments;
+                $rootScope.$apply(function () {
+                    callback.apply(socket, args);
+                });
+            });
+        }
 
-		function getDrugStoresByDrug(drugItem, callback){
-			var requestData=[
-				'',
-				'',
-				drugItem.cdprep,
-				drugItem.cdform,
-				"все",
-				"Все",
-				"",
-				"",,,,,,,,,"",""]
-			socket.emit('send_gridtable4', requestData);
-			 socket.on('grid_data4', function () {  
-		        var args = arguments;
-		        $rootScope.$apply(function () {
-		          callback.apply(socket, args);
-		        });
-		      });
-		}
-	}
+        function getDrugStoresByDrug(drugItem, callback) {
+            var requestData = [
+                '',
+                '',
+                drugItem.cdprep,
+                drugItem.cdform,
+                "все",
+                "Все",
+                "",
+                "", , , , , , , , , "", ""]
+            socket.emit('send_gridtable4', requestData);
+            socket.on('grid_data4', function () {
+                var args = arguments;
+                $rootScope.$apply(function () {
+                    callback.apply(socket, args);
+                });
+            });
+        }
+
+        function getFavorites(callback) {
+            socket.emit('get_lekar_stat', '');
+            socket.on('lekar_stat_data', function () {
+                var args = arguments;
+                $rootScope.$apply(function () {
+                    callback.apply(socket, args);
+                });
+            })
+        }
+    }
 })()
--- test/unit/ServicesSpec.js
+++ test/unit/ServicesSpec.js
@@ -1,134 +1,187 @@
 'use strict';
 
-describe('Services', function() {
- 	var mock, socket, searchQueryActual='one', actualData=[1,2,3];
+describe('Services', function () {
+    var mock, socket, searchQueryActual = 'one', actualData = [1, 2, 3];
 
-	beforeEach(module('informBuroApp.core'));
-	
-	describe("Services Defined", function(){
-		beforeEach(function(){
-			inject(function(webSocket){
-				socket=webSocket;
-			});
-		});
+    beforeEach(module('informBuroApp.core'));
 
-		it("should behave defined Service", function() {
-			expect(socket).toBeDefined();
-		});
-	});
+    describe("Services Defined", function () {
+        beforeEach(function () {
+            inject(function (webSocket) {
+                socket = webSocket;
+            });
+        });
 
-	describe("Get Drugs", function(){
-		var expectedData=null;
-		beforeEach(function(done){
-			mock=(function(){
-				var callback=null;
-				var internal={
-					on:function(action,callbackAction){
-						switch (action){
-							case 'grid_data1':
-								callback=callbackAction;
-							break;
-						}
-					},
-					emit:jasmine.createSpy().and.callFake(function(){
-						setTimeout(function(){
-							if (callback)
-								callback.call(this,actualData);
-						});
-								
-					})
-				};
-				var _mock={
-					internal:internal,
-					connect:function(url){
-						return internal;
-					}
-				};
-			return _mock;
-			})();		
-			
-			module(function($provide){
-				$provide.value('$io', mock);
-			});
+        it("should behave defined Service", function () {
+            expect(socket).toBeDefined();
+        });
+    });
 
-			inject(function(webSocket){
-				socket=webSocket;
-			});	
-			socket.getDrugs(searchQueryActual, function(data){
-				expectedData=data;
-				done();
-			});	
-		});
+    describe("Get Drugs", function () {
+        var expectedData = null;
+        beforeEach(function (done) {
+            mock = (function () {
+                var callback = null;
+                var internal = {
+                    on: function (action, callbackAction) {
+                        switch (action) {
+                            case 'grid_data1':
+                                callback = callbackAction;
+                                break;
+                        }
+                    },
+                    emit: jasmine.createSpy().and.callFake(function () {
+                        setTimeout(function () {
+                            if (callback)
+                                callback.call(this, actualData);
+                        });
+
+                    })
+                };
+                var _mock = {
+                    internal: internal,
+                    connect: function (url) {
+                        return internal;
+                    }
+                };
+                return _mock;
+            })();
+
+            module(function ($provide) {
+                $provide.value('$io', mock);
+            });
+
+            inject(function (webSocket) {
+                socket = webSocket;
+            });
+            socket.getDrugs(searchQueryActual, function (data) {
+                expectedData = data;
+                done();
+            });
+        });
 
 
-		it('should send emit', function() {		
-	  		expect(mock.internal.emit).toHaveBeenCalledWith('send_gridtable1', ['', searchQueryActual,,,'все','все','','',,,,,,,,,'','']);
-		});
+        it('should send emit', function () {
+            expect(mock.internal.emit).toHaveBeenCalledWith('send_gridtable1', ['', searchQueryActual, , , 'все', 'все', '', '', , , , , , , , , '', '']);
+        });
 
-		it("should receive data", function() {			
-  			expect(expectedData).toEqual(actualData);
-		});
-	});
+        it("should receive data", function () {
+            expect(expectedData).toEqual(actualData);
+        });
+    });
 
-	describe("Get DrugStores", function() {
-		var drugDataMock= {cdform:198,cdmnn:835, cdprep:5181, form:"Т",nmmnn:"М",nmprep:"М1", prices:106},
-			expectedData=null;;
-		beforeEach(function(done){
-			mock=(function() {
-				var callback=null;
-				var internal={
-					on:function(action,callbackAction){
-						switch (action){
-							case 'grid_data4':
-								callback=callbackAction;
-							break;
-						}
-					},
-					emit:jasmine.createSpy().and.callFake(function(){
-						setTimeout(function(){
-							if (callback)
-								callback.call(this,actualData);
-						});
-								
-					})
-				};
-				var _mock={
-					internal:internal,
-					connect:function(url){
-						return internal;
-					}
-				};
-			return _mock;
-			})();
-			module(function($provide){
-				$provide.value('$io', mock);
-			});
+    describe("Get DrugStores", function () {
+        var drugDataMock = {cdform: 198, cdmnn: 835, cdprep: 5181, form: "Т", nmmnn: "М", nmprep: "М1", prices: 106},
+            expectedData = null;
+        ;
+        beforeEach(function (done) {
+            mock = (function () {
+                var callback = null;
+                var internal = {
+                    on: function (action, callbackAction) {
+                        switch (action) {
+                            case 'grid_data4':
+                                callback = callbackAction;
+                                break;
+                        }
+                    },
+                    emit: jasmine.createSpy().and.callFake(function () {
+                        setTimeout(function () {
+                            if (callback)
+                                callback.call(this, actualData);
+                        });
 
-			inject(function(webSocket){
-				socket=webSocket;
-			});	
-			socket.getDrugStoresByDrug(drugDataMock, function(data){
-				expectedData=data;
-				done();
-			});	
-		});
+                    })
+                };
+                var _mock = {
+                    internal: internal,
+                    connect: function (url) {
+                        return internal;
+                    }
+                };
+                return _mock;
+            })();
+            module(function ($provide) {
+                $provide.value('$io', mock);
+            });
 
-		
+            inject(function (webSocket) {
+                socket = webSocket;
+            });
+            socket.getDrugStoresByDrug(drugDataMock, function (data) {
+                expectedData = data;
+                done();
+            });
+        });
 
-		it("should send emit", function() {
-			expect(mock.internal.emit).toHaveBeenCalledWith('send_gridtable4', [
-				'',
-				'',
-				drugDataMock.cdprep,
-				drugDataMock.cdform,
-				"все",
-				"Все",
-				"",
-				"",,,,,,,,,"",""]);
-		});
 
-		it("should receive data", function() {
-			expect(expectedData).toEqual(actualData);
-		});
-	});	
+        it("should send emit", function () {
+            expect(mock.internal.emit).toHaveBeenCalledWith('send_gridtable4', [
+                '',
+                '',
+                drugDataMock.cdprep,
+                drugDataMock.cdform,
+                "все",
+                "Все",
+                "",
+                "", , , , , , , , , "", ""]);
+        });
+
+        it("should receive data", function () {
+            expect(expectedData).toEqual(actualData);
+        });
+    });
+
+    describe("Get Favorites", function () {
+        var expectedData = null;
+        beforeEach(function (done) {
+            mock = (function () {
+                var callback = null;
+                var internal = {
+                    on: function (action, callbackAction) {
+                        switch (action) {
+                            case 'lekar_stat_data':
+                                callback = callbackAction;
+                                break;
+                        }
+                    },
+                    emit: jasmine.createSpy().and.callFake(function () {
+                        setTimeout(function () {
+                            if (callback)
+                                callback.call(this, actualData);
+                        });
+
+                    })
+                };
+                var _mock = {
+                    internal: internal,
+                    connect: function (url) {
+                        return internal;
+                    }
+                };
+                return _mock;
+            })();
+
+            module(function ($provide) {
+                $provide.value('$io', mock);
+            });
+
+            inject(function (webSocket) {
+                socket = webSocket;
+            });
+
+            socket.getFavorites(function (data) {
+                expectedData = data;
+                done();
+            });
+        });
+
+        it('should send emit', function () {
+            expect(mock.internal.emit).toHaveBeenCalledWith('get_lekar_stat','');
+        });
+
+        it("should receive data", function () {
+            expect(expectedData).toEqual(actualData);
+        });
+    });
 });
